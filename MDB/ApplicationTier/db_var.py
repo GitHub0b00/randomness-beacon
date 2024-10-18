485e96ed02ab1438af8d9e9422e86d5e828681c5
GitHub0b00 <yusong@u.nus.edu>
2024-10-18 12:43:31 +0800

    code for the beacon application

--- MDB/ApplicationTier/db_var.py
+++ MDB/ApplicationTier/db_var.py
@@ -1,3 +1,3 @@
-ip_address = 'localhost'
+ip_address = 'a.b.c.d'  # The database tier machine private ip in the virtual private network. The "a.b.c.d" address is an example as a place holder.
 database_name = 'deployment'
 collection_name = '2024_June18_0'
---
+++ MDB/DatabaseTier/db_var.py
@@ -0,0 +1,3 @@
+ip_address = 'localhost'
+database_name = 'deployment'
+collection_name = '2024_June18_0'
