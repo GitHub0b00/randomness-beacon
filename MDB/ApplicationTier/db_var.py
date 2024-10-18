2fa5c7447206944aa4c0a1aec26a5418ead47e4e
GitHub0b00 <yusong@u.nus.edu>
2024-10-18 12:55:29 +0800

    Update db_var.py

--- MDB/ApplicationTier/db_var.py
+++ MDB/ApplicationTier/db_var.py
@@ -1,3 +1,3 @@
-ip_address = 'a.b.c.d'  # The database tier machine private ip in the virtual private network. The "a.b.c.d" address is an example as a place holder.
+ip_address = 'a.b.c.d:xxxxx'  # The database tier machine private ip in the virtual private network and the port number that the MongoDB is bind to. The "a.b.c.d:xxxxx" address is an example as a place holder.
 database_name = 'deployment'
 collection_name = '2024_June18_0'
